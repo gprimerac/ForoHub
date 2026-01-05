7ccb981f0547a73a02bfa98526b732a8f6f3cf6c
gprimerac <soporteconnectsys@gmail.com>
2026-01-05 17:06:05 -0300

    AutenticacionService.java

--- Challenge/Challenge/src/main/java/ForoHub/Challenge/Repositorio/AutenticacionService.java
+++ Challenge/Challenge/src/main/java/ForoHub/Challenge/Repositorio/AutenticacionService.java
@@ -1,19 +1,24 @@
 package ForoHub.Challenge.Repositorio;
 
+import ForoHub.Challenge.Repositorio.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
-@Service // Esta anotación es clave para que Spring la reconozca
+@Service
 public class AutenticacionService implements UserDetailsService {
 
     @Autowired
-    private UsuarioRepository repository;
+    private UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByLogin(username);
+        var usuario = usuarioRepository.findByLogin(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+        return usuario;
     }
 }
